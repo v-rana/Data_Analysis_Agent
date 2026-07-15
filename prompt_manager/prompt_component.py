3f61c98a59c3cdd30d5609731871fccdb4eeda31
vrana <varunrana74@gmail.com>
2026-07-15 07:51:24 +0530

    feat: prompt manager

---
+++ prompt_manager/prompt_component.py
@@ -0,0 +1,26 @@
+import os
+from typing import Union, Optional
+
+from langchain_core.prompts import ChatPromptTemplate
+
+
+
+PROMPT_FOLDER = os.getenv('PROMPT_FOLDER','prompts')
+
+# one function to read a prompt based on name
+def read_prompt(p_name: str) -> str:
+    file_name = PROMPT_FOLDER+ '/' + p_name+'.txt'
+    with open(file_name) as f:
+        prompt = f.read()
+    return prompt.strip()
+
+# function to get the full prompt template
+def get_prompt_template(system_prompt_name:str,user_template_name: str ) -> ChatPromptTemplate:
+    system_prompt = read_prompt(system_prompt_name)
+    user_prompt_template = read_prompt(user_template_name)
+    messages = [ ('system',system_prompt) , 
+                ('user',user_prompt_template)]
+    
+    template = ChatPromptTemplate.from_messages(messages)
+    return template
+
---
+++ tests/test_prompt_manager.py
@@ -0,0 +1,7 @@
+# template = get_prompt_template('sql_agent_system_prompt','sql_agent_user_template')
+# print(template)
+# params = {"schema_name":"test","table_name":"var",
+# "user_input":"give table details","table_schema":"table structure"}
+
+# result = template.invoke(params)
+# print(result)
